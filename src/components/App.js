0d97e721dae80da771168c649772c9b334fe4769
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-15 14:55:56 +0000

    some bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
+import { useState } from 'react';
+
 const App = () => {
 
+  const [count,setcount]=useState(0);
+
   const handleDoubleClick = (event) =>{
-  
+    if(count === 2) {
+      console.log("I was double clicked");
+      setcount(0);
+      
+  } else {
+      console.log("I was not double clicked");
+  }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick} onClick={()=>setcount(count+1)}>Double click me</button>
     </div>
   )
 }
