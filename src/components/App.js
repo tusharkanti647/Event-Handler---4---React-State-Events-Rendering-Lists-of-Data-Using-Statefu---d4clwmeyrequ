ea165fa084c03a89cd79464e9838d1072f01d905
tushar <tusharkanti647@gmail.com>
2023-06-01 12:16:45 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,17 @@ import { useState } from 'react';
 
 const App = () => {
 
-  const [count,setcount]=useState(0);
 
-  const handleDoubleClick = (event) =>{
-    if(count === 2) {
+  const handleDoubleClick = (event) => {
+    if (event.detail === 2) {
       console.log("I was double clicked");
-      setcount(0);
-      
-  } else {
+    } else {
       console.log("I was not double clicked");
-  }
+    }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn" onDoubleClick={handleDoubleClick} onClick={()=>setcount(count+1)}>Double click me</button>
+      <button id="dblclick-btn" onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
